de5aadbcedd294c9e0e93f2534be778ed70e6d9d
user <user@user.com>
2021-11-09 20:27:42 +0800

    all in XQ4AI

--- XQ4ROS.cpp
+++ XQ4ROS.cpp
@@ -9,70 +9,103 @@ public://XQ4Node
 	XQ4IO xq4io;
 	XQ4ROS(string port = "", string nn = "XQ4Server", string ns = "XQ") : Node(nn, ns) 
 	{ 
-		if (port.empty()) return;
-		if (xq4io.open(port)) spdlog::info("Open {} failed", port);
+		if (port.empty()) { stats.data = "Error: serial port cannot be empty and try again by service"; return; }
+		if (xq4io.open(port)) { stats.data = fmt::format("Error: open {} failed and try again by service", port); }
+		else stats.data = fmt::format("Info: open {} done", port);
+		pubXQ4Status->publish(stats); spdlog::info(stats.data);
 	}
 
-public://ManuPort
+public://PubStatus
+	std_msgs::msg::String stats;
+	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pubXQ4Status = create_publisher<std_msgs::msg::String>("XQ4Status", 10);
+
+public://ManuPort //ros2 service call /XQ/ClosePort std_srvs/srv/Trigger "{}"
 	rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr srvClosePort = create_service<std_srvs::srv::Trigger>("ClosePort",
 		[this](const std_srvs::srv::Trigger::Request::SharedPtr req, std_srvs::srv::Trigger::Response::SharedPtr res)->void
-		{
-			res->message = "Close " + xq4io.name() + " succeeded";
-			if (xq4io.opened()) xq4io.close();
-			else res->message = xq4io.name() + " not opened";
+		{		
+			if (xq4io.opened()) { xq4io.close(); stats.data = fmt::format("Info: Close {} done", xq4io.name()); }
+			else stats.data = fmt::format("Warn: {} not opened", xq4io.name());
+			res->message = stats.data; pubXQ4Status->publish(stats); spdlog::info(stats.data);
 		});
 
 	//ros2 service call /XQ/OpenPort ams_xq/srv/XQ4Serve "{str: 'COM2'}"
 	rclcpp::Service<ams_xq::srv::XQ4Serve>::SharedPtr srvManuPort = create_service<ams_xq::srv::XQ4Serve>("OpenPort",
 		[this](const ams_xq::srv::XQ4Serve::Request::SharedPtr req, ams_xq::srv::XQ4Serve::Response::SharedPtr res)->void
 		{
-			if (xq4io.opened()) res->msg = xq4io.name() + " not closed";
-			else res->msg = "Open " + xq4io.name() + (xq4io.open(req->cmd) ? " succeeded" : " failed and check whether to set right port name");
+			if (xq4io.opened()) stats.data = fmt::format("Warn: {} not closed", xq4io.name());
+			else if (xq4io.open(req->cmd)) stats.data = fmt::format("Info: open {} done", xq4io.name());
+			else stats.data = fmt::format("Error: open {} failed", xq4io.name());
+			res->msg = stats.data; pubXQ4Status->publish(stats); spdlog::info(stats.data);
 		});
 
-public://ManuCar
+public://ManuCar //ros2 service call /XQ/OpenPort ams_xq/srv/XQ4Serve "{str: 'T'}" //T/R/I
 	rclcpp::Service<ams_xq::srv::XQ4Serve>::SharedPtr srvSetMode = create_service<ams_xq::srv::XQ4Serve>("SetMode",
 		[this](const ams_xq::srv::XQ4Serve::Request::SharedPtr req, ams_xq::srv::XQ4Serve::Response::SharedPtr res)->void
 		{
-			res->msg = "Done";
-			xq4io.setMode(req->cmd[0]);
+			if (req->cmd.size() != 1) stats.data = "Error: wrong format";
+			else
+			{
+				xq4io.setMode(req->cmd[0]);
+				stats.data = fmt::format("Info: SetMode({:x}) done", req->cmd[0]);
+				res->msg = stats.data; pubXQ4Status->publish(stats); spdlog::info(stats.data);
+			}
 		});
 
 	//ros2 service call /XQ/OpenPort ams_xq/srv/XQ4Serve "{str: '1'}" //0/1/2
 	rclcpp::Service<ams_xq::srv::XQ4Serve>::SharedPtr srvRunSensor = create_service<ams_xq::srv::XQ4Serve>("RunSensor",
 		[this](const ams_xq::srv::XQ4Serve::Request::SharedPtr req, ams_xq::srv::XQ4Serve::Response::SharedPtr res)->void
 		{
-			res->msg = "Done";
-			xq4io.runSensor(int(req->cmd[0]) - 48);
+			if (req->cmd.size() != 1) stats.data = "Error: wrong format";
+			else
+			{
+				xq4io.runSensor(int(req->cmd[0]) - 48);
+				stats.data = fmt::format("Info: RunSensor({:x}) done", req->cmd[0]);
+				res->msg = stats.data; pubXQ4Status->publish(stats); spdlog::info(stats.data);
+			}
 		});
 
 	//ros2 service call -r 1 /XQ/RunMotor ams_xq/srv/XQ4Serve "{str: 'FF22'}" //forward50 //SS22 for brake50
 	rclcpp::Service<ams_xq::srv::XQ4Serve>::SharedPtr srvRunMotor = create_service<ams_xq::srv::XQ4Serve>("RunMotor",
 		[this](const ams_xq::srv::XQ4Serve::Request::SharedPtr req, ams_xq::srv::XQ4Serve::Response::SharedPtr res)->void
 		{
-			res->msg = "Done";
-			if (req->cmd.size() > 3) xq4io.runMotor(req->cmd[0], req->cmd[1], req->cmd[2], req->cmd[3]);
-			else res->msg = "Failed (too few params)";
+			if (req->cmd.size() != 3) stats.data = "Error: wrong format";
+			else
+			{
+				xq4io.runMotor(req->cmd[0], req->cmd[1], req->cmd[2], req->cmd[3]);
+				stats.data = fmt::format("Info: RunMotor({:x}-{:x}-{:x}) done", req->cmd[0], req->cmd[2], req->cmd[3]);
+			}
+			res->msg = stats.data; pubXQ4Status->publish(stats); spdlog::info(stats.data);
 		});
 
 	//ros2 topic pub -r 1 /XQ/RunCar std_msgs/msg/String "{data: 'f2'}" //forward50 //s2 for brake50
 	rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subRunCar = create_subscription<std_msgs::msg::String>("RunCar", 10,
 		[&](const std_msgs::msg::String::SharedPtr val)->void 
 		{ 
-			if(val->data.size() > 1) xq4io.runCar(val->data[0], val->data[1]); 
-			else spdlog::error("Published data not enough");
+			if (val->data.size() != 2) stats.data = "Error: wrong format";
+			else xq4io.runCar(val->data[0], val->data[1]);
 		});
 
 public://Update
 	rclcpp::Publisher<ams_xq::msg::XQ4Frame>::SharedPtr pubXQFrame = create_publisher<ams_xq::msg::XQ4Frame>("XQ4Frame", 10);
-    rclcpp::TimerBase::SharedPtr TimerXQFrame = create_wall_timer(50ms, [&]()->void
+    rclcpp::TimerBase::SharedPtr TimerXQFrame = create_wall_timer(10ms, [&]()->void
         {
-			if(!xq4io.opened()) { this_thread::sleep_for(2000ms); spdlog::error("No port opened"); return; }
+			if(!xq4io.opened()) 
+			{ 
+				this_thread::sleep_for(2000ms); 
+				stats.data = "Error: no port opened. Please open it by service";
+				pubXQ4Status->publish(stats); spdlog::info(stats.data);
+				return;
+			}
 			XQ4IO::XQ4Frame *frame = 0;
             xq4io.getStatus(&frame);
 			ams_xq::msg::XQ4Frame rosFrame;
 			if(frame) memcpy(&rosFrame, frame, sizeof(rosFrame));
-			else spdlog::warn("Invalid frame and it is possible to connect one wrong port if this happens continuously");
+			else
+			{
+				stats.data = "Error: invalid frame \n\t(1)It is possible to connect one wrong port if this happens continuously";
+				stats.data += fmt::format("\n\t(2) Close current {} and open the right one", xq4io.name());
+				pubXQ4Status->publish(stats); spdlog::info(stats.data);
+			}
             pubXQFrame->publish(rosFrame);
         });
 
@@ -85,12 +118,8 @@ public:
 		vector<string> portnames;
 		for (int k = 0; k < 100; ++k) portnames.push_back("/dev/ttyUSB" + std::to_string(k));
 		for (int k = 0; k < 100; ++k) portnames.push_back("COM" + std::to_string(k));
-		for (int k = 0; k < portnames.size(); ++k)
-			if (server->xq4io.open(portnames[k])) break;
-		if(!server->xq4io.opened()) { spdlog::error("Open port failed"); }
-		else spdlog::info("{} has opened successfully", server->xq4io.name());
+		for (int k = 0; k < portnames.size(); ++k) if (server->xq4io.open(portnames[k])) break;
 		rclcpp::spin(server);
-		
 		rclcpp::shutdown();	
 	}
 };
